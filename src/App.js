0f82b75363d1c6161cfe102ef678e4196ea4ad60
datobobokhia <dato.bobokhia@gmail.com>
2023-03-29 16:42:41 +0400

    resolved conflicts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { GlobalStyle } from "./components/styles/Global";
+import PageContent from "./components/PageContent";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState, createContext } from "react";
 
-function App() {
+export const myContext = createContext();
+
+export default function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("/data.json");
+      setData(response.data);
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Router>
+        <myContext.Provider value={data}>
+          <Routes>
+            <Route path="/" element={<PageContent />} />
+            {data.map((item, index) => (
+              <Route
+                path={`/${item.name.toLowerCase()}`}
+                element={<PageContent />}
+                key={index}
+              />
+            ))}
+          </Routes>
+        </myContext.Provider>
+      </Router>
+    </>
   );
 }
-
-export default App;
